85a87a8ec83a28c94868297dfc57348d13cd672f
Alonso Ayala <alayor3@gmail.com>
2015-08-15 20:58:29 +0000

    Missing files.

--- api/src/main/test/com/lealpoints/controller/api/ClientControllerTest.java
+++ api/src/main/test/com/lealpoints/controller/api/ClientControllerTest.java
@@ -2,7 +2,8 @@ package com.lealpoints.controller.api;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.lealpoints.model.ClientPoints;
+import com.lealpoints.model.Client;
+import com.lealpoints.model.CompanyClientMapping;
 import com.lealpoints.service.ClientService;
 import com.lealpoints.service.model.ClientRegistration;
 import com.lealpoints.service.model.ServiceResult;
@@ -35,54 +36,58 @@ public class ClientControllerTest {
 
     @Test
     public void testGetByCompanyId() throws Exception {
-        List<ClientPoints> expectedClients = new ArrayList<>();
-        expectedClients.add(createClientPoints(100, "123"));
-        expectedClients.add(createClientPoints(200, "456"));
-        final ServiceResult<List<ClientPoints>> expectedServiceResult = new ServiceResult<>(true, "1", expectedClients);
+        List<CompanyClientMapping> expectedClients = new ArrayList<>();
+        expectedClients.add(createCompanyClientMapping(100, "123"));
+        expectedClients.add(createCompanyClientMapping(200, "456"));
+        final ServiceResult<List<CompanyClientMapping>> expectedServiceResult = new ServiceResult<>(true, "1", expectedClients);
         final ClientService clientService = createClientServiceForGet(expectedServiceResult);
         final ClientController clientController = new ClientController(clientService);
 
-        ResponseEntity<ServiceResult<List<ClientPoints>>> responseEntity = clientController.getByCompanyId(1);
+        ResponseEntity<ServiceResult<List<CompanyClientMapping>>> responseEntity = clientController.getByCompanyId(1);
         assertNotNull(responseEntity);
         ServiceResult actualServiceResults = responseEntity.getBody();
         assertNotNull(actualServiceResults);
         assertEquals(expectedServiceResult.isSuccess(), actualServiceResults.isSuccess());
         assertEquals(expectedServiceResult.getMessage(), actualServiceResults.getMessage());
         assertNotNull(expectedServiceResult.getObject());
-        List<ClientPoints> actualClients = expectedServiceResult.getObject();
-        assertEquals(2, actualClients.size());
-        assertEquals(100, actualClients.get(0).getPoints(), 0.00);
-        assertEquals("123", actualClients.get(0).getPhone());
-        assertEquals(200, actualClients.get(1).getPoints(), 0.00);
-        assertEquals("456", actualClients.get(1).getPhone());
+        List<CompanyClientMapping> companyClientMappings = expectedServiceResult.getObject();
+        assertEquals(2, companyClientMappings.size());
+        assertEquals(100, companyClientMappings.get(0).getPoints(), 0.00);
+        assertEquals("123", companyClientMappings.get(0).getClient().getPhone());
+        assertEquals(200, companyClientMappings.get(1).getPoints(), 0.00);
+        assertEquals("456", companyClientMappings.get(1).getClient().getPhone());
     }
 
     @Test
     public void testGetByCompanyIdPhone() throws Exception {
-        ClientPoints clientPoints = new ClientPoints();
-        clientPoints.setPoints(1200);
-        clientPoints.setPhone("1234567890");
-        final ServiceResult<ClientPoints> expectedServiceResult = new ServiceResult<>(true, "1", clientPoints);
+        CompanyClientMapping companyClientMapping = new CompanyClientMapping();
+        companyClientMapping.setPoints(1200);
+        Client client = new Client();
+        client.setPhone("1234567890");
+        companyClientMapping.setClient(client);
+        final ServiceResult<CompanyClientMapping> expectedServiceResult = new ServiceResult<>(true, "1", companyClientMapping);
         final ClientService clientService = createClientServiceForGetByCompanyIdPhone(expectedServiceResult);
         final ClientController clientController = new ClientController(clientService);
 
-        ResponseEntity<ServiceResult<ClientPoints>> responseEntity = clientController.getByCompanyIdPhone(1, "1234567890");
+        ResponseEntity<ServiceResult<CompanyClientMapping>> responseEntity = clientController.getByCompanyIdPhone(1, "1234567890");
         assertNotNull(responseEntity);
         ServiceResult actualServiceResults = responseEntity.getBody();
         assertNotNull(actualServiceResults);
         assertEquals(expectedServiceResult.isSuccess(), actualServiceResults.isSuccess());
         assertEquals(expectedServiceResult.getMessage(), actualServiceResults.getMessage());
         assertNotNull(expectedServiceResult.getObject());
-        ClientPoints actualClientPoints = expectedServiceResult.getObject();
-        assertEquals(1200, actualClientPoints.getPoints(), 0.00);
-        assertEquals("1234567890", actualClientPoints.getPhone());
+        CompanyClientMapping actualCompanyClientMapping = expectedServiceResult.getObject();
+        assertEquals(1200, actualCompanyClientMapping.getPoints(), 0.00);
+        assertEquals("1234567890", actualCompanyClientMapping.getClient().getPhone());
     }
 
-    private ClientPoints createClientPoints(float points, String phone) {
-        ClientPoints clientPoints = new ClientPoints();
-        clientPoints.setPoints(points);
-        clientPoints.setPhone(phone);
-        return clientPoints;
+    private CompanyClientMapping createCompanyClientMapping(float points, String phone) {
+        CompanyClientMapping companyClientMapping = new CompanyClientMapping();
+        companyClientMapping.setPoints(points);
+        Client client = new Client();
+        client.setPhone(phone);
+        companyClientMapping.setClient(client);
+        return companyClientMapping;
     }
 
     private ClientService createClientServiceForRegister(ServiceResult<Long> serviceResult) throws Exception {
@@ -92,14 +97,14 @@ public class ClientControllerTest {
         return clientService;
     }
 
-    private ClientService createClientServiceForGet(ServiceResult<List<ClientPoints>> serviceResult) throws Exception {
+    private ClientService createClientServiceForGet(ServiceResult<List<CompanyClientMapping>> serviceResult) throws Exception {
         final ClientService clientService = EasyMock.createMock(ClientService.class);
         expect(clientService.getByCompanyId(anyLong())).andReturn(serviceResult);
         replay(clientService);
         return clientService;
     }
 
-    private ClientService createClientServiceForGetByCompanyIdPhone(ServiceResult<ClientPoints> serviceResult) throws Exception {
+    private ClientService createClientServiceForGetByCompanyIdPhone(ServiceResult<CompanyClientMapping> serviceResult) throws Exception {
         final ClientService clientService = EasyMock.createMock(ClientService.class);
         expect(clientService.getByCompanyIdPhone(anyLong(), anyString())).andReturn(serviceResult);
         replay(clientService);
