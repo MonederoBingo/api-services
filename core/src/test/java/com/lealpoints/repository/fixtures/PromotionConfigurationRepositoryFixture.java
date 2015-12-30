2891278ad28589c4e82c5f98b3e971ae44accac6
Fernando Calderon <fercalderon37@gmail.com>
2015-12-30 10:58:41 -0700

    id: 22-Change-sql-unit-test-fixtures-from-sql-files-to-java-variables desc: reformat static variables reviewer:aayala

--- core/src/test/java/com/lealpoints/repository/fixtures/PromotionConfigurationRepositoryFixture.java
+++ core/src/test/java/com/lealpoints/repository/fixtures/PromotionConfigurationRepositoryFixture.java
@@ -4,8 +4,8 @@ public class PromotionConfigurationRepositoryFixture {
 
     public static final String INSERT_COMPANY_ANDO_PROMOTION_CONFIGURATION =
             "INSERT INTO company (company_id, name, url_image_logo) VALUES (1, 'X', 'Y');" +
-                    "INSERT INTO promotion_configuration (promotion_configuration_id, company_id, description, required_points) " +
-                    "VALUES (1, 1, '10% off', 1200);";
+                    "INSERT INTO promotion_configuration (promotion_configuration_id, company_id, " +
+                    "description, required_points) VALUES (1, 1, '10% off', 1200);";
 
     public static final String INSERT_COMPANY_AND_TWO_PROMOTION_CONFIGURATION =
             "INSERT INTO company (company_id, name, url_image_logo) VALUES (1, 'X', 'Y');" +
