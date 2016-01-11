4ef55d736099885fb8256b922e3be1f4d70677b5
Fernando Calderon <fercalderon37@gmail.com>
2016-01-10 17:27:09 -0700

    id: 27-Create-web-service-method-list-in-Controller-CompanyUserController-and-return-a-list-with-the-current-user-of-the-company desc: list method created and its test reviewer:aayala

--- api/src/main/java/com/lealpoints/controller/api/v1/CompanyUserController.java
+++ api/src/main/java/com/lealpoints/controller/api/v1/CompanyUserController.java
@@ -1,19 +1,21 @@
 package com.lealpoints.controller.api.v1;
 
-        import com.lealpoints.controller.base.BaseController;
-        import com.lealpoints.service.model.CompanyUserRegistration;
-        import com.lealpoints.service.response.ServiceMessage;
-        import com.lealpoints.service.response.ServiceResult;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.RequestBody;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
-
-        import javax.ws.rs.Produces;
-        import javax.ws.rs.core.MediaType;
-
-        import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import com.lealpoints.controller.base.BaseController;
+import com.lealpoints.service.model.CompanyUserRegistration;
+import com.lealpoints.service.response.ServiceMessage;
+import com.lealpoints.service.response.ServiceResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping("/v1/company_users")
@@ -22,6 +24,17 @@ public class CompanyUserController extends BaseController {
     @RequestMapping(value="/register", method = POST, headers = ACCEPT_HEADER)
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseEntity<ServiceResult> register(@RequestBody CompanyUserRegistration companyUserRegistration) {
-        return new ResponseEntity<>(new ServiceResult(false,new ServiceMessage("Operation not supported yet")), HttpStatus.OK);
+        return new ResponseEntity<>(new ServiceResult(false,new ServiceMessage("Operation not supported yet")),
+                HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/{companyId}", method = GET, headers = ACCEPT_HEADER)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseEntity<ServiceResult<List<String>>> get(@PathVariable("companyId") Long companyId) {
+        List<String> namesList = new ArrayList<>();
+        namesList.add("none");
+        return new ResponseEntity<>(new ServiceResult<>(false,new ServiceMessage("Operation not supported yet"),
+                namesList), HttpStatus.OK);
+    }
+
 }
--- api/src/test/java/com/lealpoints/controller/api/v1/CompanyUserControllerTest.java
+++ api/src/test/java/com/lealpoints/controller/api/v1/CompanyUserControllerTest.java
@@ -4,14 +4,19 @@ import com.lealpoints.service.model.CompanyUserRegistration;
 import com.lealpoints.service.response.ServiceMessage;
 import com.lealpoints.service.response.ServiceResult;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class CompanyUserControllerTest {
 @Test
 public void testRegister() throws Exception {
-    final ServiceResult expectedServiceResult = new ServiceResult(false, new ServiceMessage("Operation not supported yet"));
+    final ServiceResult expectedServiceResult = new ServiceResult(false,
+            new ServiceMessage("Operation not supported yet"));
     final CompanyUserRegistration companyUserRegistration = new CompanyUserRegistration();
     companyUserRegistration.setName("name");
     companyUserRegistration.setEmail("email@test.com");
@@ -21,5 +26,22 @@ public void testRegister() throws Exception {
     assertNotNull(actualServiceResults);
     assertEquals(expectedServiceResult.isSuccess(), actualServiceResults.isSuccess());
     assertEquals(expectedServiceResult.getMessage(), actualServiceResults.getMessage());
-}
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        List<String> testList = new ArrayList<>();
+        testList.add("none");
+        ServiceResult<List<String>> expectedServiceResult  = new ServiceResult<>(true,
+                new ServiceMessage("Operation not supported yet"), testList );
+        ResponseEntity<ServiceResult<List<String>>> responseEntity = new ResponseEntity<>
+                (expectedServiceResult,HttpStatus.OK);
+        assertNotNull(responseEntity);
+        ServiceResult actualServiceResults = responseEntity.getBody();
+        assertNotNull(actualServiceResults);
+        assertEquals(expectedServiceResult.isSuccess(), actualServiceResults.isSuccess());
+        assertEquals(expectedServiceResult.getMessage(), actualServiceResults.getMessage());
+        assertNotNull(actualServiceResults.getObject());
+    }
+
 }
