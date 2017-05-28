942311b00ee354b1e7bc7dd396c0982adb75af24
alayor <alayor3@gmail.com>
2017-05-28 15:26:52 -0500

    Converting to a Spring Boot project.

---
+++ api/src/main/java/com/monederobingo/App.java
@@ -0,0 +1,14 @@
+package com.monederobingo;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
+{
+    public static void main(String[] args)
+    {
+        run(App.class, args);
+    }
+}
--- start_services.cmd
+++ start_services.cmd
@@ -1,4 +1,5 @@
 start cmd /k ..\configuration-service\start.bat
 ping 127.0.0.1 -n 10 > nul
+start cmd /k ..\database-service\start.bat
 start cmd /k ..\authorization-service\start.bat
 start cmd /k ..\points-configuration-service\start.bat
